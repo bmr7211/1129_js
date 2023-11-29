b00e5e03d50c3787ad7b81f4154ad8476ded9885
bmr7211 <133488663+bmr7211@users.noreply.github.com>
2023-11-29 12:38:01 +0900

    Add files via upload

---
+++ 1129.js
@@ -0,0 +1,25 @@
+/*
+let number = new Number(273);
+let string = new String('안녕하세요');
+let boolean = new Boolean(true);
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+
+let number_1 = 273.5210332;
+
+console.log(number_1.toFixed(1));
+console.log(number_1.toFixed(4));
+
+let string = 'abcdef';
+
+string = string.toUpperCase();
+console.log(string);
+
+let string = '안녕하세요. 좋은 아침입니다.';
+
+if (string.indexOf('아침') >= 0) {
+    console.log('좋은 아침이예요...!');
+}
+*/
